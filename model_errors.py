8a28bf433332133b53f917a0c32f0b54cab25e10
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2023-12-01 18:19:50 +0100

    powers of 10 were miswritten in parameter warning handler

--- model_errors.py
+++ model_errors.py
@@ -23,11 +23,11 @@ def check_in_range(params):
     pi0, pi1, pi2, alpha1, alpha2, alpha3, alpha2b, alpha3b, beta1, beta2, \
         gamma2, gamma3, delta0, delta2, tauC, tauA1, tauA2, tauCA1, tauCA2 = params
     if not (tauA1 == 0.3): warnings.warn('tauA1 is not 0.3', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= pi0 <= 1): warnings.warn('pi0 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= pi1 <= 1): warnings.warn('pi1 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= pi2 <= 1): warnings.warn('pi2 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-6 <= gamma2 <= 1): warnings.warn('gamma2 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-6 <= gamma3 <= 1): warnings.warn('gamma3 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= pi0 <= 1): warnings.warn('pi0 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= pi1 <= 1): warnings.warn('pi1 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= pi2 <= 1): warnings.warn('pi2 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-6 <= gamma2 <= 1): warnings.warn('gamma2 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-6 <= gamma3 <= 1): warnings.warn('gamma3 not in range', ParameterRangeWarning, stacklevel=2)
     if not (0 <= beta1 <= 1/tauA1): warnings.warn('beta1 not in range', ParameterRangeWarning, stacklevel=2)
     if not (0 <= beta2 <= 1/tauA1): warnings.warn('beta2 not in range', ParameterRangeWarning, stacklevel=2)
     if not (0 <= delta0 <= 10): warnings.warn('delta0 not in range', ParameterRangeWarning, stacklevel=2)
@@ -36,11 +36,11 @@ def check_in_range(params):
     if not (0 <= tauA2 <= 2): warnings.warn('tauA2 not in range', ParameterRangeWarning, stacklevel=2)
     if not (0.3 <= tauCA1 <= 2): warnings.warn('tauCA1 not in range', ParameterRangeWarning, stacklevel=2)
     if not (0 <= tauCA2 <= 2): warnings.warn('tauCA2 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= alpha1 <= 1): warnings.warn('alpha1 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= alpha2 <= 1): warnings.warn('alpha2 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= alpha3 <= 1): warnings.warn('alpha3 not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= alpha2b <= 1): warnings.warn('alpha2b not in range', ParameterRangeWarning, stacklevel=2)
-    if not (10e-5 <= alpha3b <= 1): warnings.warn('alpha3b not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= alpha1 <= 1): warnings.warn('alpha1 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= alpha2 <= 1): warnings.warn('alpha2 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= alpha3 <= 1): warnings.warn('alpha3 not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= alpha2b <= 1): warnings.warn('alpha2b not in range', ParameterRangeWarning, stacklevel=2)
+    if not (1e-5 <= alpha3b <= 1): warnings.warn('alpha3b not in range', ParameterRangeWarning, stacklevel=2)
     
 default_hook=sys.excepthook
 def exception_handler(exception_type, exception, traceback): 
